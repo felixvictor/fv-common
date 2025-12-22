f62c8869a38c8e0a73429a4a4fb1f1cf59d20b51
ibey <ibey@byrtz.de>
2025-12-22 10:02:25 +0100

    feat: na related stuff wip

--- src/na/time.ts
+++ src/na/time.ts
@@ -1,5 +1,28 @@
+import dayjs from "dayjs"
+import "dayjs/locale/de"
+import "dayjs/locale/en"
+import "dayjs/locale/en-gb"
+import utc from "dayjs/plugin/utc"
+
 import { formatTimeRange } from "../date/date.js"
+import { getLocale, onLocaleChange, setLocale as setLibraryLocale } from "../locale.js"
 import { hoursPerDay, maxPortBattleHour, serverMaintenanceHour } from "./constants.js"
+
+dayjs.extend(utc)
+
+// Wrapper to sync dayjs locale with library locale
+export const setDateLocale = (locale: string): void => {
+    setLibraryLocale(locale)
+    dayjs.locale(locale)
+}
+
+// Sync dayjs locale on any locale changes
+onLocaleChange(() => {
+    dayjs.locale(getLocale())
+})
+
+// Initialise with default
+setDateLocale("en-GB")
 
 /**
  * Calculates and formats the port battle time window.
@@ -27,4 +50,48 @@ export const getPortBattleTime = (startHoursFromSMH: number, isNeutralPort = fal
     return startHoursFromSMH === 0
         ? formatTimeRange(serverMaintenanceHour, maxPortBattleHour)
         : formatTimeRange(startTime, endTime)
+}
+
+export const getToday = () => {
+    const now = dayjs.utc()
+    let begin = dayjs().utc().hour(serverMaintenanceHour).minute(0)
+    if (now.hour() < begin.hour()) {
+        begin = begin.subtract(1, "day")
+    }
+
+    const end = begin.add(1, "day")
+
+    return { begin, end }
+}
+
+export const getYesterday = () => {
+    const now = dayjs.utc()
+    let begin = now.hour(serverMaintenanceHour).minute(0).subtract(1, "day")
+    if (now.hour() < begin.hour()) {
+        begin = begin.subtract(1, "day")
+    }
+
+    const end = begin.add(1, "day")
+
+    return { begin, end }
+}
+
+export const getThisWeek = () => {
+    const currentMondayOfWeek = dayjs().utc().startOf("week")
+    // This Monday
+    const begin = currentMondayOfWeek.hour(serverMaintenanceHour)
+    // Next Monday
+    const end = currentMondayOfWeek.add(7, "day").hour(serverMaintenanceHour)
+
+    return { begin, end }
+}
+
+export const getLastWeek = () => {
+    const currentMondayOfWeek = dayjs().utc().startOf("week")
+    // Monday last week
+    const begin = currentMondayOfWeek.subtract(7, "day").hour(serverMaintenanceHour)
+    // This Monday
+    const end = currentMondayOfWeek.hour(serverMaintenanceHour)
+
+    return { begin, end }
 }

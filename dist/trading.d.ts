42c24c98fbbabb2cbf49050f5ecd67b0d821427e
ibey <ibey@byrtz.de>
2026-07-19 16:37:22 +0200

    build: dist

--- dist/trading.d.ts
+++ dist/trading.d.ts
@@ -1,10 +1,11 @@
 //#region src/trading/nyse.d.ts
 declare const isNyseTradingDay: (instant?: Temporal.Instant) => boolean;
 declare const getNyseTradingDay: (instant?: Temporal.Instant) => string;
+declare const isNyseOpen: (instant?: Temporal.Instant) => boolean;
 declare const isNysePreMarket: (instant?: Temporal.Instant) => boolean;
 declare const isNyseExtendedTradingHours: (instant?: Temporal.Instant) => boolean;
 declare const isEdgarOperating: (instant?: Temporal.Instant) => boolean;
 declare const addNyseTradingDays: (tradingDay: string, tradingDaysToAdd: number) => string;
 //#endregion
-export { addNyseTradingDays, getNyseTradingDay, isEdgarOperating, isNyseExtendedTradingHours, isNysePreMarket, isNyseTradingDay };
+export { addNyseTradingDays, getNyseTradingDay, isEdgarOperating, isNyseExtendedTradingHours, isNyseOpen, isNysePreMarket, isNyseTradingDay };
 //# sourceMappingURL=trading.d.ts.map
